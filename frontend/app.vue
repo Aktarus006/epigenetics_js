d405c422eb55f2d814c17d3d2cd039536e3dff44
Aktarus006 <joachim.bastin@diagenode.com>
2022-03-14 14:37:28 +0100

    config: nuxt3 initial setup

---
+++ frontend/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="py-20 px-10 text-center">
+    <Header />
+    <NuxtPage />
+    <Footer />
+  </main>
+</template>
+
+<style>
+@import '~/styles/main.css';
+</style>
---
+++ frontend/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <div bg="gray-700 dark:gray-200" color="white dark:gray-600">
+    Hello World
+  </div>
+</template>
